92b0d3088be61b2cb9dd95d8199a2e02e5edb8d1
Travis Dixon <the.trav@gmail.com>
2012-06-05 08:52:14 +1000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politicrime::Application.config.secret_token = 'df0622a5f08a96ff8d5a565df26b7c4552cb1e4b6327ecf3e644ed324cdb56dcf7aebdbea9c2e2b1f82d3268a4ba89e2a6404f109507dbddc11dd0d38822e3b3'
