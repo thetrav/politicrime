85fed730bc5e35dc657de08b0306d25d7dc22906
Travis Dixon <the.trav@gmail.com>
2012-07-02 08:57:21 +1000

    complete location map builder

--- app/models/location_builder.rb
+++ app/models/location_builder.rb
@@ -18,8 +18,22 @@ class LocationBuilder
     @tiles = {}
   end
 
-  def exit_at(params)
+  def exit_at(p)
+    x = p[:x]
+    y = p[:y]
+    z = p[:z]
 
+    put_if_empty x-1, y-1, z, :exit
+    put_if_empty x,   y-1, z, :exit
+    put_if_empty x+1, y-1, z, :exit
+
+    put_if_empty x-1, y, z,   :exit
+    put          x,   y, z,   :floor
+    put_if_empty x+1, y, z,   :exit
+
+    put_if_empty x-1, y+1, z, :exit
+    put_if_empty x,   y+1, z, :exit
+    put_if_empty x+1, y+1, z, :exit
   end
 
   def room_at(p)
@@ -33,26 +47,40 @@ class LocationBuilder
     yes = 0..(h-1)
     xes.each do |i|
       yes.each do |j|
-        put i+x, j+y, z, @@TILES[:floor]
+        put i+x, j+y, z, :floor
       end
     end
     xes.each do |i|
-      put i+x, y, z, @@TILES[:wall]
-      put i+x, y+h-1, z, @@TILES[:wall]
+      put i+x, y, z, :wall
+      put i+x, y+h-1, z, :wall
     end
     yes.each do |i|
-      put x, y+i, z, @@TILES[:wall]
-      put x+w-1, y+i, z, @@TILES[:wall]
+      put x, y+i, z, :wall
+      put x+w-1, y+i, z, :wall
     end
   end
 
-  def hall_at(params)
-
+  def y_hall_at(p)
+    room_at(p)
+    x = p[:x]
+    y = p[:y]
+    z = p[:z]
+    w = p[:w]
+    h = p[:h]
+    xes = 1..(w-2)
+    xes.each do |i|
+      put i+x, y, z, :floor
+      put i+x, y+h-1, z, :floor
+    end
   end
 
   private
 
+  def put_if_empty(x,y,z,tile)
+    put(x,y,z,tile) if @tiles[{:x=>x, :y=>y, :z => z}].nil?
+  end
+
   def put(x,y,z,tile)
-    @tiles[{:x=>x, :y=>y, :z => z}] = tile
+    @tiles[{:x=>x, :y=>y, :z => z}] = @@TILES[tile]
   end
 end
--- spec/location_builder_spec.rb
+++ spec/location_builder_spec.rb
@@ -1,20 +1,72 @@
 require 'spec_helper'
 
 describe LocationBuilder do
+
+  W = LocationBuilder.TILES[:wall]
+  F = LocationBuilder.TILES[:floor]
+  X = LocationBuilder.TILES[:exit]
+  N = nil
+
+  def make(values)
+    tiles = {}
+    values.each_with_index do |row, y|
+      row.each_with_index do |tile, x|
+        tiles[{:x => x, :y => y, :z => 0}] = tile
+      end
+    end
+    tiles
+  end
+
+  def stringify(map, xStart, yStart, xEnd, yEnd)
+    tiles = []
+    (yStart..yEnd).each_with_index do |y|
+      row = []
+      (xStart..xEnd).each_with_index do |x|
+        tile = map[{:x => x, :y => y, :z => 0}]
+        row << (tile.nil? ? 0 : tile)
+      end
+      tiles << row
+    end
+    strings = tiles.map {|row| row.to_s}
+    strings.join("\n")
+  end
+
+  def compare(expected, actual, x, y, xe, ye)
+    e = stringify(expected, x,y,xe,ye)
+    a = stringify(actual, x,y,xe,ye)
+    puts "expecting\n#{e}\n\ngot\n#{a}\n\n"
+    e.should eq a
+  end
+
   it "should build a room" do
     builder = LocationBuilder.new
     builder.room_at(:x => 0, :y => 0, :z => 0, :w => 3, :h => 3)
-    expected = {
-        {:x=>0,:y=>0,:z=>0} => LocationBuilder.TILES[:wall],
-        {:x=>1,:y=>0,:z=>0} => LocationBuilder.TILES[:wall],
-        {:x=>2,:y=>0,:z=>0} => LocationBuilder.TILES[:wall],
-        {:x=>0,:y=>1,:z=>0} => LocationBuilder.TILES[:wall],
-        {:x=>1,:y=>1,:z=>0} => LocationBuilder.TILES[:floor],
-        {:x=>2,:y=>1,:z=>0} => LocationBuilder.TILES[:wall],
-        {:x=>0,:y=>2,:z=>0} => LocationBuilder.TILES[:wall],
-        {:x=>1,:y=>2,:z=>0} => LocationBuilder.TILES[:wall],
-        {:x=>2,:y=>2,:z=>0} => LocationBuilder.TILES[:wall],
-    }
-    builder.tiles.should eq(expected)
+    expected = make([
+            [W, W, W],
+            [W, F, W],
+            [W, W, W],
+        ])
+    tiles = builder.tiles
+    compare(expected, tiles, 0, 0, 2, 2)
+  end
+
+  it "should build a room with a hall to an exit" do
+    builder = LocationBuilder.new
+    builder.room_at(:x => 0, :y => 3, :z => 0, :w => 5, :h => 5)
+    builder.y_hall_at(:x => 1, :y => 2, :z => 0, :w => 3, :h => 2)
+    builder.exit_at(:x => 2, :y => 1, :z => 0)
+
+    expected = make([
+        [ N, X, X, X, N ],
+        [ N, X, F, X, N ],
+        [ N, W, F, W, N ],
+        [ W, W, F, W, W ],
+        [ W, F, F, F, W ],
+        [ W, F, F, F, W ],
+        [ W, F, F, F, W ],
+        [ W, W, W, W, W ]
+    ])
+    tiles = builder.tiles
+    compare(expected, tiles, 0, 0, 4, 7)
   end
 end
